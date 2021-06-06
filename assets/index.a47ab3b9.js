8b4b19b95eb87ef0ed7baf7daff7ab6ce2e11990
buqiyuan <buqiyuan@users.noreply.github.com>
2021-06-06 09:07:51 +0000

    deploy: f084f3db61ca909d8b518725b2e57c5b77682ea1

--- assets/index.a47ab3b9.js
+++
@@ -1,1 +0,0 @@
-import{d as a,be as r,f as e,V as s,X as t,I as n}from"./vendor.c1c0e565.js";var o=a({name:"Redirect",setup(a){const o=t(),p=s();return r((()=>{const{params:a,query:r}=o,{path:e}=a;p.replace({path:"/"+(Array.isArray(e)?e.join("/"):e),query:r})})),()=>e(n,null,null)}});export default o;
